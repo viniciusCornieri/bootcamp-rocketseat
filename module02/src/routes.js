8cedfe8ee25c455f56e97c30797b0ccb970ad4a5
Vinicius Cornieri <vinicius.m.cornieri@gmail.com>
2019-08-31 14:09:16 -0300

    module02: creating UserController

--- module02/src/routes.js
+++ module02/src/routes.js
@@ -1,19 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (request, response) => {
-  try {
-    const user = await User.create({
-      name: 'Vinicius Cornieri',
-      email: 'vinicius.cornieri@gmail.com',
-      password_hash: 'testepassword',
-    });
-    return response.json(user);
-  } catch (err) {
-    return response.status(500).json({ Error: err.message });
-  }
-});
+routes.post('/users', UserController.store);
 
 export default routes;
