537f1f7e40c9d767c758d01f7a311fda457a437b
Vinicius Cornieri <vinicius.m.cornieri@gmail.com>
2019-09-02 23:15:30 -0300

    challenge02: adding authentication middleware and update for userController

--- challenge02/src/app/controllers/UserController.js
+++ challenge02/src/app/controllers/UserController.js
@@ -32,6 +32,49 @@ class UserController {
       email,
     });
   }
+
+  async update(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().notOneOf([Yup.ref('password')]) : field
+      ),
+      password: Yup.string().min(6),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (err) {
+      return response
+        .status(400)
+        .json({ error: 'Validation fails', message: err.errors });
+    }
+    const { email, oldPassword } = request.body;
+
+    const user = await User.findByPk(request.userId);
+
+    if (email !== user.email) {
+      const userExists = User.findOne({ where: { email } });
+      if (userExists) {
+        return response.status(400).json({ error: 'Email already used' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return response.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = await user.update(request.body);
+    return response.json({
+      id,
+      name,
+      email,
+    });
+  }
 }
 
 export default new UserController();
