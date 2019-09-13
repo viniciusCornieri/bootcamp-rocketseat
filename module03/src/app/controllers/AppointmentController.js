00651ef069e20b915038ae8b73a21e16101b002e
Vinicius Cornieri <vinicius.m.cornieri@gmail.com>
2019-09-12 23:52:08 -0300

    module03: creating appointment cancelling route

--- module03/src/app/controllers/AppointmentController.js
+++ module03/src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 
 import User from '../models/User';
@@ -119,6 +119,30 @@ class AppointmentController {
       content: `Novo agendamento de ${user.name} para ${formattedDate}`,
       user: provider_id,
     });
+
+    return response.json(appointment);
+  }
+
+  async delete(request, response) {
+    const appointment = await Appointment.findByPk(request.params.id);
+    /**
+     * checks if the logged user owns the appointment
+     */
+    if (appointment.user_id !== request.userId) {
+      return response.status(401).json({
+        error: "You don't have permission to cancel the appointment.",
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+    if (isBefore(dateWithSub, new Date())) {
+      return response
+        .status(401)
+        .json({ error: 'You can only cancel appointments 2 hours in advance' });
+    }
+
+    appointment.cancelled_at = new Date();
+    await appointment.save();
 
     return response.json(appointment);
   }
