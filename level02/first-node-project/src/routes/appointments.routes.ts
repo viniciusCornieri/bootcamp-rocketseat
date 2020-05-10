bf12bdb1c9f9d5374c895cbbf1e258aa550fdbc4
Vinicius Cornieri <vinicius.cornieri@matera.com>
2020-05-09 23:02:19 -0300

    level02: first-node-project - structuring the MVC

--- level02/first-node-project/src/routes/appointments.routes.ts
+++ level02/first-node-project/src/routes/appointments.routes.ts
@@ -1,22 +1,16 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
-const appointments: Appointment[] = [];
+const appointmentRepository = new AppointmentRepository();
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(appointment.date, parsedDate),
+  const findAppointmentInSameDate = appointmentRepository.findByDate(
+    parsedDate,
   );
 
   if (findAppointmentInSameDate) {
@@ -25,12 +19,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ error: 'This appointment is already booked' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
-  appointments.push(appointment);
+  const appointment = appointmentRepository.create(provider, parsedDate);
 
   return response.status(201).json(appointment);
 });
