cf6c01c2063a9ee9f9db5df3f26870c768000ca3
Vinicius Cornieri <vinicius.cornieri@matera.com>
2020-05-21 20:52:03 -0300

    level02: creating users migration

--- level02/first-node-project/src/models/User.ts
+++ level02/first-node-project/src/models/User.ts
@@ -6,22 +6,25 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
-@Entity('appointments')
-class Appointment {
+@Entity('users')
+class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
-  provider: string;
+  name: string;
 
-  @Column('timestamp with time zone')
-  date: Date;
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
 
   @CreateDateColumn()
-  created_at: Date;
+  createdAt: Date;
 
   @UpdateDateColumn()
-  update_at: Date;
+  updateAt: Date;
 }
 
-export default Appointment;
+export default User;
