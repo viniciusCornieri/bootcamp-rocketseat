9761e1248e6223f3c68291d7488ba560c9713919
Vinicius Cornieri <vinicius.cornieri@matera.com>
2020-05-24 14:08:38 -0300

    level02: first-node-project - create route to see the avatar files

--- level02/first-node-project/src/routes/index.ts
+++ level02/first-node-project/src/routes/index.ts
@@ -1,12 +1,14 @@
-import { Router } from 'express';
+import express, { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
 import usersRouter from './users.routes';
 import sessionsRouter from './sessions.routes';
+import uploadConfig from '../config/upload';
 
 const routes = Router();
 
 routes.use('/sessions', sessionsRouter);
 routes.use('/users', usersRouter);
+routes.use('/files', express.static(uploadConfig.directory));
 routes.use('/appointments', appointmentsRouter);
 
 export default routes;
