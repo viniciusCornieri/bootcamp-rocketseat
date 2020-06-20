e87113ad79872264d7b0d263bb093c0b49c283ef
Vinicius Cornieri <vinicius.cornieri@matera.com>
2020-06-20 17:58:53 -0300

    level03: go-barber-web - extracting button and input components

---
+++ level03/go-barber-web/src/components/Input/index.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { InputHTMLAttributes } from 'react';
+import { IconBaseProps } from 'react-icons';
+import { Container } from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: React.ComponentType<IconBaseProps>;
+}
+
+const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
+  <Container>
+    {Icon && <Icon size={20} />}
+    <input {...rest} />
+  </Container>
+);
+
+export default Input;
--- level03/go-barber-web/src/pages/SignIn/index.tsx
+++ level03/go-barber-web/src/pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => (
@@ -12,11 +14,16 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu logon</h1>
 
-        <input placeholder="E-mail" />
+        <Input name="email" placeholder="E-mail" icon={FiLock} />
 
-        <input type="password" placeholder="Senha" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Senha"
+          icon={FiMail}
+        />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
         <a href="forgot">Esqueci minha senha</a>
       </form>
       <a href="sign up">
