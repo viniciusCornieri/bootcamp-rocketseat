44b88fb0ba278cd075e915105ec15518507262ab
Vinicius Cornieri <vinicius.cornieri@matera.com>
2020-06-21 19:10:53 -0300

    level03: go-barber-web - creating sign up page

--- level03/go-barber-web/src/pages/SignUp/index.tsx
+++ level03/go-barber-web/src/pages/SignUp/index.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import { FiArrowLeft, FiMail, FiLock } from 'react-icons/fi';
+import { FiMail, FiUser, FiLock, FiArrowLeft } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
-const SignIn: React.FC = () => (
+const SignUp: React.FC = () => (
   <Container>
+    <Background />
     <Content>
       <img src={logoImg} alt="GoBarber" />
 
       <form>
-        <h1>Faça seu logon</h1>
+        <h1>Faça seu cadastro</h1>
 
+        <Input name="nome" placeholder="Nome" icon={FiUser} />
         <Input name="email" placeholder="E-mail" icon={FiMail} />
-
         <Input
           name="password"
           type="password"
@@ -23,16 +24,14 @@ const SignIn: React.FC = () => (
           icon={FiLock}
         />
 
-        <Button type="submit">Entrar</Button>
-        <a href="forgot">Esqueci minha senha</a>
+        <Button type="submit">Cadastrar</Button>
       </form>
       <a href="sign up">
         <FiArrowLeft />
-        Criar conta
+        Voltar para o logon
       </a>
     </Content>
-    <Background />
   </Container>
 );
 
-export default SignIn;
+export default SignUp;
