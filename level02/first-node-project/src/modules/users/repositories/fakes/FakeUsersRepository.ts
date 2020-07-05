e031629e5c609396060d6d9e9d317eed9b083039
Vinicius Cornieri <vinicius.cornieri@matera.com>
2020-07-05 17:08:20 -0300

    level04: architeture and tdd - adding createUserService tests

---
+++ level02/first-node-project/src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,39 @@
+import { uuid } from 'uuidv4';
+
+import IUser from '@modules/users/entities/IUser';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import IUsersRepository from '../IUsersRepository';
+
+export default class FakeUsersRepository implements IUsersRepository {
+  users: IUser[] = [];
+
+  async findById(id: string): Promise<IUser | undefined> {
+    return this.users.find(user => user.id === id);
+  }
+
+  async findByEmail(email: string): Promise<IUser | undefined> {
+    return this.users.find(user => user.email === email);
+  }
+
+  async create({ name, email, password }: ICreateUserDTO): Promise<IUser> {
+    const user: IUser = {
+      id: uuid(),
+      createdAt: new Date(),
+      name,
+      email,
+      password,
+    };
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  async save(user: IUser): Promise<IUser> {
+    const findIndex = this.users.findIndex(findUser => findUser.id === user.id);
+
+    this.users[findIndex] = user;
+
+    return user;
+  }
+}
---
+++ level02/first-node-project/src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,41 @@
+import AppError from '@shared/errors/AppError';
+import CreateUserService from './CreateUserService';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+
+describe('CreateUser', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUserService = new CreateUserService(fakeUsersRepository);
+
+    const user = await createUserService.execute({
+      name: 'John Doe',
+      email: 'john.doe@fakemail.com',
+      password: '123456',
+    });
+
+    expect(user).toHaveProperty('id');
+    expect(user).toHaveProperty('name', 'John Doe');
+    expect(user).toHaveProperty('email', 'john.doe@fakemail.com');
+    expect(user).toHaveProperty('password');
+    expect(user).toHaveProperty('createdAt');
+  });
+
+  it('should not be able to create two users with the same email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUserService = new CreateUserService(fakeUsersRepository);
+
+    await createUserService.execute({
+      name: 'John Doe',
+      email: 'john.doe@fakemail.com',
+      password: '123456',
+    });
+
+    await expect(
+      createUserService.execute({
+        name: 'John Doe 2',
+        email: 'john.doe@fakemail.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
