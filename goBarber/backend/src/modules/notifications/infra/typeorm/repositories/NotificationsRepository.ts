5c06bb066727d4300ad46eac5ebe5b0387483d54
Vinicius Cornieri <vinicius.cornieri@matera.com>
2020-07-21 23:07:49 -0300

    level04: gobarber backend - adding mongo and creating NotificationRepository

---
+++ goBarber/backend/src/modules/notifications/infra/typeorm/repositories/NotificationsRepository.ts
@@ -0,0 +1,26 @@
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import ICreateNotificationDTO from '@modules/notifications/dtos/ICreateNotificationDTO';
+import { getMongoRepository, MongoRepository } from 'typeorm';
+import Notification from '../schemas/Notification';
+
+class NotificationsRepository implements INotificationsRepository {
+  private ormRepository: MongoRepository<Notification>;
+
+  constructor() {
+    this.ormRepository = getMongoRepository(Notification, 'mongo');
+  }
+
+  public async create({
+    recipientId,
+    content,
+  }: ICreateNotificationDTO): Promise<Notification> {
+    const notification = this.ormRepository.create({
+      recipientId,
+      content,
+    });
+
+    return this.ormRepository.save(notification);
+  }
+}
+
+export default NotificationsRepository;
