c7afe6c883f07ec00c1ba53709615f34a446333d
Vinicius Cornieri <vinicius.cornieri@matera.com>
2020-09-20 19:16:42 -0300

    level05: finishing goBarber app - creating header from create apppointment page

--- goBarber/mobile/src/pages/CreateAppointment/index.tsx
+++ goBarber/mobile/src/pages/CreateAppointment/index.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { View } from 'react-native';
+import { useAuth } from '../../hooks/auth';
+
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+} from './styles';
+
+interface routeParams {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
-  return <View />;
+  const { user } = useAuth();
+  const route = useRoute();
+  const { providerId } = route.params as routeParams;
+
+  const { goBack } = useNavigation();
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+          <HeaderTitle>Cabeleireiros</HeaderTitle>
+        </BackButton>
+
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+    </Container>
+  );
 };
 
 export default CreateAppointment;
